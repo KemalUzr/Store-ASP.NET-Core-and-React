79f7c65860b997109599e0405374dacf64be8502
Ali <alikemaluzar@gmail.com>
2023-11-28 18:24:53 +0100

    Added darkmode

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,18 +1,30 @@
 import './styles.css'
 import Catalog from '../../features/catalog/Catalog';
-import { Container, CssBaseline } from '@mui/material';
+import { Container, CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 import Header from './header';
+import { useState } from 'react';
 
 function App() {
+  const[darkMode, setDarkMode] = useState(false);
+  const paletteType=darkMode ? 'dark' : 'light';
+  const theme = createTheme({
+    palette: {
+      mode: paletteType
+    }
+  })
+
+  function handleThemeChange(){
+    setDarkMode(!darkMode);
+  }
   
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline/>
-      <Header/>
+      <Header darkMode={darkMode} themeChange={handleThemeChange}/> 
       <Container>
         <Catalog/>  
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 
--- client/src/app/layout/header.tsx
+++ client/src/app/layout/header.tsx
@@ -1,12 +1,19 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { DarkMode } from "@mui/icons-material";
+import { AppBar, Switch, ToggleButton, ToggleButtonGroup, Toolbar, Typography } from "@mui/material";
 
-export default function Header() {
+interface Props {
+    darkMode: boolean;
+    themeChange: () => void;
+}
+
+export default function Header({darkMode, themeChange}: Props) {
     return (
         <AppBar position="static" sx={{mb:4}}>
             <Toolbar>
                 <Typography variant="h4">
                     Shop
                 </Typography>
+                <Switch checked={darkMode} onChange={themeChange}/>
             </Toolbar>
         </AppBar>
     )
